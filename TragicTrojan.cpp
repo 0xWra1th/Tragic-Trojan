55ceae1974eea37b10459b67b72c365f8009b815
Tetalesti <danielscragg@outlook.com>
2021-09-29 14:54:29 +0200

    Pausing, shaders and textures

--- TragicTrojan.cpp
+++ TragicTrojan.cpp
@@ -19,9 +19,25 @@ int main()
     int leftScore = 0;
     int rightScore = 0;
     bool right = false;
+    bool paused = false;
     float BALL_SPEED = 10.f;
     float BALL_Y_VELOCITY = 0.f;
     float PLAYER_SPEED = 10.f;
+    // -----------------------------------
+
+    // ------------ POST FX --------------
+    if (!sf::Shader::isAvailable())
+    {
+        std::cout << "ERR: Shaders not supported!" << std::endl;
+    }
+
+    sf::Shader shader;
+    if (!shader.loadFromFile("vertex-shader.glsl", "fragment-shader.glsl"))
+    {
+        std::cout << "ERR: Failed to load shaders!" << std::endl;
+    }
+
+    shader.setUniform("texture", sf::Shader::CurrentTexture);
     // -----------------------------------
     
     // ------------- AUDIO ---------------
@@ -56,7 +72,7 @@ int main()
     left_score.setFillColor(sf::Color::White);
     left_score.setOrigin(50.f, 50.f);
     left_score.setCharacterSize(100);
-    left_score.setPosition(100.f, 50.f);
+    left_score.setPosition(150.f, 50.f);
     std::string score = std::to_string(leftScore);
     left_score.setString(score);
 
@@ -66,41 +82,57 @@ int main()
     right_score.setFillColor(sf::Color::White);
     right_score.setOrigin(50.f, 50.f);
     right_score.setCharacterSize(100);
-    right_score.setPosition(window.getSize().x-50.f, 50.f);
+    right_score.setPosition(window.getSize().x-100.f, 50.f);
     score = std::to_string(rightScore);
     right_score.setString(score);
+
+    sf::Text pausedText;
+    pausedText.setStyle(sf::Text::Bold);
+    pausedText.setFont(font);
+    pausedText.setFillColor(sf::Color::White);
+    pausedText.setOrigin(150.f, 50.f);
+    pausedText.setCharacterSize(100);
+    pausedText.setPosition(window.getSize().x / 2.f, window.getSize().y / 2.f);
+    pausedText.setString("PAUSED");
     // -----------------------------------
 
     // ------------- OBJECTS -------------
+    // The Textures
+    sf::Texture ballTexture;
+    ballTexture.loadFromFile("assets/ball.jpg", sf::IntRect());
+
+    sf::Texture backgroundTexture;
+    backgroundTexture.loadFromFile("assets/background.jpg", sf::IntRect());
+
+    sf::Texture playerTexture;
+    playerTexture.loadFromFile("assets/texture.jpg", sf::IntRect());
+
     // BALL
     sf::CircleShape ball(20.f);
     ball.setFillColor(sf::Color::White);
     ball.setOrigin(20.f, 20.f);
-    ball.setPosition(950.f , window.getSize().y / 2.f);    
-
-    // DIVIDER
-    sf::RectangleShape divider(sf::Vector2f(2.f, window.getSize().y));
-    divider.setFillColor(sf::Color::White);
-    divider.setOrigin(0.f, window.getSize().y / 2.f);
-    divider.setPosition(window.getSize().x / 2.f, window.getSize().y / 2.f);
+    ball.setPosition(950.f , window.getSize().y / 2.f);
+    ball.setTexture(&ballTexture, false);
 
     // BACKGROUND
     sf::RectangleShape background(sf::Vector2f(window.getSize().x, window.getSize().y));
-    background.setFillColor(sf::Color::Blue);
     background.setOrigin(window.getSize().x / 2.f, window.getSize().y / 2.f);
     background.setPosition(window.getSize().x / 2.f, window.getSize().y / 2.f);
+    background.setTexture(&backgroundTexture, false);
 
     // PLAYER LEFT
     sf::RectangleShape player_left(sf::Vector2f(20.f, 200.f));
     player_left.setFillColor(sf::Color::White);
     player_left.setOrigin(10.f, 100.f);
     player_left.setPosition(225.f , window.getSize().y / 2.f);
+    player_left.setTexture(&playerTexture, false);
 
     // PLAYER RIGHT
     sf::RectangleShape player_right(sf::Vector2f(20.f, 200.f));
     player_right.setFillColor(sf::Color::White);
     player_right.setOrigin(10.f, 100.f);
     player_right.setPosition(window.getSize().x - 225.f, window.getSize().y / 2.f);
+    player_right.setTexture(&playerTexture, false);
     // -----------------------------------
 
     // ------------ GAME LOOP ------------
@@ -128,113 +160,148 @@ int main()
                     score = std::to_string(rightScore);
                     right_score.setString(score);
                     ball.setPosition(window.getSize().x / 2, window.getSize().y / 2);
+                    player_left.setPosition(225.f , window.getSize().y / 2.f);
+                    player_right.setPosition(window.getSize().x - 225.f, window.getSize().y / 2.f);
                     BALL_Y_VELOCITY = 0;
+                    window.clear();
+                    window.draw(background, &shader);
+                    window.draw(ball, &shader);
+                    window.draw(player_left, &shader);
+                    window.draw(player_right, &shader);
+                    window.draw(left_score, &shader);
+                    window.draw(right_score, &shader);
+                    window.draw(pausedText, &shader);
+                    window.display();
+                }else if(event.key.code == sf::Keyboard::P){
+                    // Reset
+                    if(paused){
+                        paused = false;
+                        window.clear();
+                        window.draw(background, &shader);
+                        window.draw(ball, &shader);
+                        window.draw(player_left, &shader);
+                        window.draw(player_right, &shader);
+                        window.draw(left_score, &shader);
+                        window.draw(right_score, &shader);
+                        window.display();
+                    }else{
+                        paused = true;
+                        window.clear();
+                        window.draw(background, &shader);
+                        window.draw(ball, &shader);
+                        window.draw(player_left, &shader);
+                        window.draw(player_right, &shader);
+                        window.draw(left_score, &shader);
+                        window.draw(right_score, &shader);
+                        window.draw(pausedText, &shader);
+                        window.display();
+                    }
                 }
             }
         }
         // -----------------------------------
-
-        // ----------- KEY PRESSES -----------
-        // Left Player
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::W)){
-            player_left.setPosition(player_left.getPosition().x , player_left.getPosition().y - (PLAYER_SPEED * dt.asSeconds() * 100));
-        }else if(sf::Keyboard::isKeyPressed(sf::Keyboard::S)){
-            player_left.setPosition(player_left.getPosition().x , player_left.getPosition().y + (PLAYER_SPEED * dt.asSeconds() * 100));
-        }
-
-        // Right Player
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up)){
-            player_right.setPosition(player_right.getPosition().x , player_right.getPosition().y - (PLAYER_SPEED * dt.asSeconds() * 100));
-        }else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down)){
-            player_right.setPosition(player_right.getPosition().x , player_right.getPosition().y + (PLAYER_SPEED * dt.asSeconds() * 100));
-        }
-        // -----------------------------------
-
-        // ---------- CLAMP POSTION ----------
-        //Left Player
-        if(player_left.getPosition().y < 125.f){
-            player_left.setPosition(player_left.getPosition().x, 125.f);
-        }
-
-        if(player_left.getPosition().y > 775.f){
-            player_left.setPosition(player_left.getPosition().x, 775.f);
-        }
-
-        // Right Player
-        if(player_right.getPosition().y < 125.f){
-            player_right.setPosition(player_right.getPosition().x, 125.f);
-        }
-
-        if(player_right.getPosition().y > 775.f){
-            player_right.setPosition(player_right.getPosition().x, 775.f);
-        }
-        // -----------------------------------
-
-        // ------------ COLLISIONS -----------
-        // Player hitting ball
-        if(player_left.getGlobalBounds().intersects(ball.getGlobalBounds())){
-            right = true;
-            float diff = ball.getPosition().y - player_left.getPosition().y;
-            BALL_Y_VELOCITY = diff / 10;
-            hitSound.play();
-        }else if(player_right.getGlobalBounds().intersects(ball.getGlobalBounds())){
-            right = false;
-            float diff = ball.getPosition().y - player_right.getPosition().y;
-            BALL_Y_VELOCITY = diff / 10;
-            hitSound.play();
-        }
-
-        // Ball bouncing off walls
-        if(ball.getPosition().y < 10.f){
-            BALL_Y_VELOCITY *= -1;
-            wallSound.play();
-        }else if(ball.getPosition().y > 890){
-            BALL_Y_VELOCITY *= -1;
-            wallSound.play();
-        }
-        // -----------------------------------
-
-        // ---------- SCORE UPDATE -----------
-        if(ball.getPosition().x > (window.getSize().x - 50.f)){
-            leftScore++;
-            score = std::to_string(leftScore);
-            left_score.setString(score);
-            ball.setPosition(window.getSize().x / 2, window.getSize().y / 2);
-            right = false;
-            BALL_Y_VELOCITY = 0;
-            scoreSound.play();
-        }else if(ball.getPosition().x < 50.f){
-            rightScore++;
-            score = std::to_string(rightScore);
-            right_score.setString(score);
-            ball.setPosition(window.getSize().x / 2, window.getSize().y / 2);
-            right = true;
-            BALL_Y_VELOCITY = 0;
-            scoreSound.play();
-        }
-        // -----------------------------------
-
-        // ------------ MOVE BALL ------------
-        if(ball.getPosition().x < 1600 & ball.getPosition().x > 0){
-            if(right){
-                ball.setPosition(ball.getPosition().x + (BALL_SPEED * dt.asSeconds() * 100), ball.getPosition().y + (BALL_Y_VELOCITY * dt.asSeconds() * 100));
-            }else{
-                ball.setPosition(ball.getPosition().x - (BALL_SPEED * dt.asSeconds() * 100), ball.getPosition().y + (BALL_Y_VELOCITY * dt.asSeconds() * 100));
+        if(!paused){
+            // ----------- KEY PRESSES -----------
+            // Left Player
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::W)){
+                player_left.setPosition(player_left.getPosition().x , player_left.getPosition().y - (PLAYER_SPEED * dt.asSeconds() * 100));
+            }else if(sf::Keyboard::isKeyPressed(sf::Keyboard::S)){
+                player_left.setPosition(player_left.getPosition().x , player_left.getPosition().y + (PLAYER_SPEED * dt.asSeconds() * 100));
             }
-        }
-        // -----------------------------------
 
-        // ---------- UPDATE WINDOW ----------
-        window.clear();
-        window.draw(background);
-        window.draw(divider);
-        window.draw(ball);
-        window.draw(player_left);
-        window.draw(player_right);
-        window.draw(left_score);
-        window.draw(right_score);
-        window.display();
-        // -----------------------------------
+            // Right Player
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up)){
+                player_right.setPosition(player_right.getPosition().x , player_right.getPosition().y - (PLAYER_SPEED * dt.asSeconds() * 100));
+            }else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down)){
+                player_right.setPosition(player_right.getPosition().x , player_right.getPosition().y + (PLAYER_SPEED * dt.asSeconds() * 100));
+            }
+            // -----------------------------------
+
+            // ---------- CLAMP POSTION ----------
+            //Left Player
+            if(player_left.getPosition().y < 125.f){
+                player_left.setPosition(player_left.getPosition().x, 125.f);
+            }
+
+            if(player_left.getPosition().y > 775.f){
+                player_left.setPosition(player_left.getPosition().x, 775.f);
+            }
+
+            // Right Player
+            if(player_right.getPosition().y < 125.f){
+                player_right.setPosition(player_right.getPosition().x, 125.f);
+            }
+
+            if(player_right.getPosition().y > 775.f){
+                player_right.setPosition(player_right.getPosition().x, 775.f);
+            }
+            // -----------------------------------
+
+            // ------------ COLLISIONS -----------
+            // Player hitting ball
+            if(player_left.getGlobalBounds().intersects(ball.getGlobalBounds())){
+                right = true;
+                float diff = ball.getPosition().y - player_left.getPosition().y;
+                BALL_Y_VELOCITY = diff / 10;
+                hitSound.play();
+            }else if(player_right.getGlobalBounds().intersects(ball.getGlobalBounds())){
+                right = false;
+                float diff = ball.getPosition().y - player_right.getPosition().y;
+                BALL_Y_VELOCITY = diff / 10;
+                hitSound.play();
+            }
+
+            // Ball bouncing off walls
+            if(ball.getPosition().y < 10.f){
+                BALL_Y_VELOCITY *= -1;
+                wallSound.play();
+            }else if(ball.getPosition().y > 890){
+                BALL_Y_VELOCITY *= -1;
+                wallSound.play();
+            }
+            // -----------------------------------
+
+            // ---------- SCORE UPDATE -----------
+            if(ball.getPosition().x > (window.getSize().x - 50.f)){
+                leftScore++;
+                score = std::to_string(leftScore);
+                left_score.setString(score);
+                ball.setPosition(window.getSize().x / 2, window.getSize().y / 2);
+                right = false;
+                BALL_Y_VELOCITY = 0;
+                scoreSound.play();
+            }else if(ball.getPosition().x < 50.f){
+                rightScore++;
+                score = std::to_string(rightScore);
+                right_score.setString(score);
+                ball.setPosition(window.getSize().x / 2, window.getSize().y / 2);
+                right = true;
+                BALL_Y_VELOCITY = 0;
+                scoreSound.play();
+            }
+            // -----------------------------------
+
+            // ------------ MOVE BALL ------------
+            if(ball.getPosition().x < 1600 & ball.getPosition().x > 0){
+                if(right){
+                    ball.setPosition(ball.getPosition().x + (BALL_SPEED * dt.asSeconds() * 100), ball.getPosition().y + (BALL_Y_VELOCITY * dt.asSeconds() * 100));
+                }else{
+                    ball.setPosition(ball.getPosition().x - (BALL_SPEED * dt.asSeconds() * 100), ball.getPosition().y + (BALL_Y_VELOCITY * dt.asSeconds() * 100));
+                }
+            }
+            // -----------------------------------
+
+            // ---------- UPDATE WINDOW ----------
+            window.clear();
+            window.draw(background, &shader);
+            window.draw(ball, &shader);
+            window.draw(player_left, &shader);
+            window.draw(player_right, &shader);
+            window.draw(left_score, &shader);
+            window.draw(right_score, &shader);
+            window.display();
+            // -----------------------------------
+        }
     }
     // -----------------------------------
 
